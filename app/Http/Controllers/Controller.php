214105d6655fa58a91e6313a555cb0d093bdb286
rafaelareann <rafaelareanndavid@gmail.com>
2026-04-07 23:14:50 +0800

    Update: Errors Fixed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,18 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Recipe;
-use Illuminate\Http\Request;
-
-class RecipeController extends Controller
+abstract class Controller
 {
-    /**
-     * Fetch all recipes from the database and pass them to the Blade view.
-     * This fulfills the requirement: "Pass data from your model to your Blade views"
-     */
-    public function index()
-    {
-        $recipes = Recipe::all();
-        return view('recipes.index', compact('recipes'));
-    }
+    //
 }
